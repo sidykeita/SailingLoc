49cfaf508fe2b87489d0e65761e4e60dd3181797
John Doe <john.doe@example.com>
2025-07-16 23:55:21 +0200

    Ajout de crud SailingLoc

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./src/models/db');
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+connectDB();
+
+// Routes
+const authRoutes = require('./src/routes/authRoutes');
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API SailingLoc fonctionne ! 🚀');
+  });
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("API SailingLoc opérationnelle ✅");
-});
-
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur lancé sur le port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Serveur lancé sur le port ${PORT}`));
