79201f1cb68e099351f355cfb744ce2783bc2a36
John Doe <john.doe@example.com>
2025-08-16 20:49:47 +0200

    connexion en ligne

--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://sailingadmin:sailingpass@mongo:27017/sailingloc?authSource=admin', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected: mongo');
+    console.log('✅ Connecté à MongoDB :', process.env.MONGO_URI);
   } catch (error) {
-    console.error(error);
+    console.error('❌ Erreur de connexion à MongoDB :', error.message);
     process.exit(1);
   }
 };
